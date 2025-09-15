92708f5a5c2e3fe94595870ee8595a9ad44e4876
MAKRI <31958699+alimakri@users.noreply.github.com>
2025-09-15 12:59:34 +0200

    Add files via upload

---
+++ 1.ps1
@@ -0,0 +1,9 @@
+﻿$p1 = New-Object -TypeName PSCustomObject -Property @{ Nom=  "MAKRI"; Prenom='Ali'; Ville="Lyon"}
+$p2 = New-Object -TypeName PSCustomObject -Property @{ Nom=  "Dupont"; Prenom='Henri'; Ville="Lyon"}
+$o1 = @{}
+$listep = @()
+$listep += $p1
+$listep += $p2
+
+$listep[1].Prenom
+
---
+++ 2.ps1
@@ -0,0 +1,16 @@
+﻿# Get-Process | Where-Object {$_.ProcessName -eq 'svchost' }
+# Get-Service | where {$_.Status -eq 'Running' }
+
+# Get-Service | Where-Object { $_.Status -eq 'Running' -and $_.Name -like '!A*' }
+
+Get-Service | Where-Object {
+    $_.Status -eq 'Running' -and $_.Name -match '^A.*'
+}
+
+Get-Service | Select-Object Name, Status | Export-Csv -Path "d:\serv.csv"
+
+$servicesImport = Import-Csv -Path "d:\serv.csv"
+
+$servicesImport | gm
+
+Get-Service | Format-Wide
