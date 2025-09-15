eb2f8c9ebfa22bbf647b0d18d87dee0a977e4f29
MAKRI <31958699+alimakri@users.noreply.github.com>
2025-09-15 16:43:02 +0200

    Add files via upload

---
+++ 10.ps1
@@ -0,0 +1,37 @@
+﻿Clear-Host
+$pointsJoueur = 0
+$pointsMachine = 0
+
+# $listeChoix = @("Aucun";"Pierre";"Feuille";"Ciseau")
+enum ChoixEnum {
+    Pierre = 1
+    Feuille = 2
+    Ciseau = 3
+}
+
+while($pointsJoueur -lt 3 -and $pointsMachine -lt 3){
+    Write-host "Faîtes un choix : `n1.Pierre `n2.Feuille `n3.Ciseau" -ForegroundColor Green
+    $choixJoueur = [ChoixEnum] (Read-Host) 
+
+    $choixMachine = [ChoixEnum] (Get-Random -Minimum 1 -Maximum 4)
+
+
+    if ($choixJoueur -eq $choixMachine){
+        $pointsJoueur=0
+        $pointsMachine =0
+    }
+    elseif (
+        ($choixMachine -eq 1 -and $choixJoueur -eq 3) -or
+        ($choixMachine -eq 2 -and $choixJoueur -eq 1) -or
+        ($choixMachine -eq 3 -and $choixJoueur -eq 2)
+        ){
+        $pointsMachine++;
+        $pointsJoueur=0;
+        }
+    else{
+        $pointsJoueur++;
+        $pointsMachine=0;
+    }
+    write-host "$choixMachine - $choixJoueur"
+    Write-host "Machine: $pointsMachine - Joueur: $pointsJoueur"
+}
